485eff17318b6aa8965fbdcd43fed0d176ee008a
jb7959 <jb7959@naver.com>
2018-06-20 17:32:35 +0900

    구분자 추가 및 파싱 완료

--- app.js
+++ app.js
@@ -41,16 +41,25 @@ app.use(function(err, req, res, next) {
 
 var request = require('request');
 const cheerio = require('cheerio')
-request('https://www.melon.com/mymusic/like/mymusiclikesong_list.htm?memberKey=5221201', function (error, response, body) {
+request('https://www.melon.com/mymusic/like/mymusiclikesong_listPaging.htm?startIndex=0&pageSize=20&memberKey=5221201&orderBy=UPDT_DATE', function (error, response, body) {
     //console.log('error:', error); // Print the error if one occurred
     //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     //console.log('body:', body); // Print the HTML for the 멜론 homepage.
     const $ = cheerio.load(body);
     //내가 좋아요를 누른 가수 목록;
-    $('option').each(function(index, ele){
-      console.log(ele)
+    const class_b = $('a', $('.ellipsis'));
+    //https://www.npmjs.com/package/cheerio  // API참조
+    class_b.each(function(index){
+      ++index; // 숫자를 읽기 쉽게 1을 더함
+      //index 1:상세페이지로감, 2:곡명, 3:가수명, 4:가수명, 5:앨범명 ==> 이번의 경우 2,4,5 필요
+        //이후는 6,7,8,9,10인데.. 이것을 5로 나눠서 나머지로 구분하겠다. ==> 곡명은 2, 가수명은 4, 앨범명은 0
+        if(index%5 === 2){console.log($(this).text());} // 곡명
+        if(index%5 === 4){console.log($(this).text());} // 가수명
+        if(index%5 === 0){console.log($(this).text());} // 앨범명
     });
 });
+//https://www.melon.com/mymusic/like/mymusiclikesong_list.htm?memberKey=5221201
+//https://www.melon.com/mymusic/like/mymusiclikesong_listPaging.htm?startIndex=0&pageSize=20&memberKey=5221201&orderBy=UPDT_DATE
 
 
 
