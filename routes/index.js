adc739fdc3f957ae28c7b28e7e6813dba2b889a5
jb7959 <jb7959@naver.com>
2018-06-22 09:46:01 +0900

    모듈 분리 및 URI 매핑 1. 리스트 검색부분의 모듈화 수행 2. URI 부분의 라우팅을 통한 JSON으로의 반환

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+//내가 만든 좋아요 크롤러 호출
+var findMymusic = require('../private_modules/findMyFavoriteMusic.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+    var memberKey = 5221201;
+    findMymusic.countMytotalFavoriteSongs(memberKey).then(count => findMymusic.findMyList(count,memberKey)).then(mySongList => res.json(mySongList)); // express의 JSON을 사용하여 Array[Object]를 변경
 });
+
+
+var memberKey = 5221201;
+/*********나의 좋아요 곡 수량을 세기 -> 총 수량만큼 목록구하기 -> 갯수 출력************/
+//countMytotalFavoriteSongs(memberKey).then(count => findMyList(count,memberKey)).then(mySongList => mySongList.forEach((element, index, array) => console.log('a[' + index + '] = ' + element)));
+//findMymusic.countMytotalFavoriteSongs(memberKey)//.then(count => findMymusic.findMyList(count,memberKey)).then(mySongList => JSON.stringify(mySongList)); // JSON.stringify를 사용하여 JSON to array로 변경
 
 module.exports = router;
