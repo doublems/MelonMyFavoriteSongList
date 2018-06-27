cec8642bd57dbeb20c6fc11385ec3e51a3a016e3
jb7959 <jb7959@naver.com>
2018-06-27 17:26:32 +0900

    URI 디자인 적용 및 README.MD 적용

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/my-song-list/:memberKey', function(req, res, next) {
 });
 
 
-var memberKey = 5221201;
+//var memberKey = 5221201;
 /*********나의 좋아요 곡 수량을 세기 -> 총 수량만큼 목록구하기 -> 갯수 출력************/
 //countMytotalFavoriteSongs(memberKey).then(count => findMyList(count,memberKey)).then(mySongList => mySongList.forEach((element, index, array) => console.log('a[' + index + '] = ' + element)));
 //findMymusic.countMytotalFavoriteSongs(memberKey)//.then(count => findMymusic.findMyList(count,memberKey)).then(mySongList => JSON.stringify(mySongList)); // JSON.stringify를 사용하여 JSON to array로 변경
